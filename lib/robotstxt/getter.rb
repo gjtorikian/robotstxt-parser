61a174e8b2f1fe35d34609a15dae4df5ef95029b
Garen Torikian <gjtorikian@gmail.com>
2014-09-17 17:46:03 -0700

    Quickfix for somehow empty strings

--- lib/robotstxt/getter.rb
+++ lib/robotstxt/getter.rb
@@ -69,7 +69,7 @@ module Robotstxt
     # In the case that we can't decode, Ruby's laissez faire attitude to encoding
     # should mean that we have a reasonable chance of working anyway.
     def decode_body(response)
-      return nil if response.body.nil?
+      return "" if response.body.blank? || response.body.nil?
       Robotstxt.ultimate_scrubber(response.body)
     end
 
